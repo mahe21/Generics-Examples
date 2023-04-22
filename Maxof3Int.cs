59c6a6370532db0da230c46090f7cedfd596d961
Mahesh <Maheshk2104@gmail.com>
2023-04-23 01:34:04 +0530

    Firest commit

---
+++ Maxof3Int.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics1
+{
+    internal class Maxof3Int
+    {
+        public static T MaxofThree<T>(T a, T b, T c) where T : IComparable<T>
+        {
+            T max = a;
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0) 
+            { 
+                max = c;
+            }
+            return max;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Generics1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maxof3Int maxof3Int = new Maxof3Int();
+
+            //TC1
+            int Maximum = Maxof3Int.MaxofThree(12, 23, 45);
+            Console.WriteLine("Max value is :" +Maximum);
+
+            //TC2
+            double Maximum2 = Maxof3Int.MaxofThree(21.4, 34.2, 1.2);
+            Console.WriteLine("Max value is :" + Maximum2);
+
+            //TC3
+            float Maximum3 = (float)Maxof3Int.MaxofThree(21.4, 20, 10.2);
+            Console.WriteLine("Max value is :" + Maximum3);
+
+
+
+        }
+    }
+}
