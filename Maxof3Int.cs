7669bf0d7e1bb6ba74a5ab095a928ddf373fee4d
Mahesh <Maheshk2104@gmail.com>
2023-04-23 02:00:54 +0530

    Changed to int data type

--- Maxof3Int.cs
+++ Maxof3Int.cs
@@ -8,19 +8,33 @@ namespace Generics1
 {
     internal class Maxof3Int
     {
-        public static T MaxofThree<T>(T a, T b, T c) where T : IComparable<T>
+        static void Main()
         {
-            T max = a;
-            if (b.CompareTo(max) > 0)
+            //TC1
+            int MaxNum1 = MaxofThree(30, 20, 10);
+            Console.WriteLine("Max Num is :"+MaxNum1);
+            //TC2
+            int MaxNum2 = MaxofThree(30, 40, 20);
+            Console.WriteLine("Max Num is :" + MaxNum2);
+            //TC3
+            int MaxNum3 = MaxofThree(30, 40, 50);
+            Console.WriteLine("Max Num is :" + MaxNum3);
+        }
+
+        public static int MaxofThree(int num1, int num2, int num3)
+        {
+            int MaxOfNum = num1;
+            if (num2.CompareTo(MaxOfNum) > 0)
             {
-                max = b;
+                MaxOfNum = num2;
             }
-            if (c.CompareTo(max) > 0) 
-            { 
-                max = c;
+            if (num3.CompareTo(MaxOfNum) > 0)
+            {
+                MaxOfNum = num3;
             }
-            return max;
+            return MaxOfNum;
         }
 
     }
+
 }
