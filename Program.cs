bb662309c2fcfc5c7a48ddbfade81654029fcbb4
Mahesh <Maheshk2104@gmail.com>
2023-04-23 01:39:20 +0530

    Changed to int values

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@
             Maxof3Int maxof3Int = new Maxof3Int();
 
             //TC1
-            int Maximum = Maxof3Int.MaxofThree(12, 23, 45);
+            int Maximum = Maxof3Int.MaxofThree(40, 23, 15);
             Console.WriteLine("Max value is :" +Maximum);
 
             //TC2
-            double Maximum2 = Maxof3Int.MaxofThree(21.4, 34.2, 1.2);
+            int Maximum2 = Maxof3Int.MaxofThree(21, 34, 1);
             Console.WriteLine("Max value is :" + Maximum2);
 
             //TC3
-            float Maximum3 = (float)Maxof3Int.MaxofThree(21.4, 20, 10.2);
+            int Maximum3 = Maxof3Int.MaxofThree(19, 20, 45);
             Console.WriteLine("Max value is :" + Maximum3);
 
 
